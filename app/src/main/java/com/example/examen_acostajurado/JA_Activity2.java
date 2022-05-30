830c488efa73df46cbac4e907f644d5c33aefbca
baae29 <68650006+baae29@users.noreply.github.com>
2022-05-30 18:30:23 -0500

    Commit 6:30

--- app/src/main/java/com/example/examen_acostajurado/JA_Activity2.java
+++ app/src/main/java/com/example/examen_acostajurado/JA_Activity2.java
@@ -3,12 +3,20 @@ package com.example.examen_acostajurado;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class JA_Activity2 extends AppCompatActivity {
-
+    private EditText editTextnumeros;
+    private ListView listViewdatos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ja2);
+
+        editTextnumeros = findViewById(R.id.JBBA_editTextIngreseunNumero);
+        listViewdatos = findViewById(R.id.JBBA_ListViewNumeros);
     }
+
+
 }
