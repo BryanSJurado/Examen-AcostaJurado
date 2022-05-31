a83e4ddaa117d0fa33e072cd3209de6585bd4644
baae29 <68650006+baae29@users.noreply.github.com>
2022-05-30 19:01:28 -0500

    Commit 7:00

--- app/src/main/java/com/example/examen_acostajurado/JA_Activity2.java
+++ app/src/main/java/com/example/examen_acostajurado/JA_Activity2.java
@@ -3,12 +3,19 @@ package com.example.examen_acostajurado;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JA_Activity2 extends AppCompatActivity {
     private EditText editTextnumeros;
     private ListView listViewdatos;
+    private Button buttonIngresar
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +23,23 @@ public class JA_Activity2 extends AppCompatActivity {
 
         editTextnumeros = findViewById(R.id.JBBA_editTextIngreseunNumero);
         listViewdatos = findViewById(R.id.JBBA_ListViewNumeros);
+        buttonIngresar = findViewById(R.id.JBBA_buttonIngresar);
+
+        buttonIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1, JBBA_ingresarDatos());
+        listViewdatos.setAdapter(adapter);
+
     }
 
+    private List<String> JBBA_ingresarDatos(){
+        List<String> list = new ArrayList<String>();
+        list.add(editTextnumeros.getText().toString());
+        return list;
 
+    }
 }
